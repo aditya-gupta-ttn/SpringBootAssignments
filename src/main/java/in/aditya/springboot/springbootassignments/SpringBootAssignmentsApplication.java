2a2d32228d5570d0783d23654f65aba6a0aa69fd
aditya-gupta-ttn <aditya.gupta1@tothenew.com>
2025-03-05 23:19:33 +0530

    This is main Commit

---
+++ src/main/java/in/aditya/springboot/springbootassignments/SpringBootAssignmentsApplication.java
@@ -0,0 +1,13 @@
+package in.aditya.springboot.springbootassignments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAssignmentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAssignmentsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/in/aditya/springboot/springbootassignments/SpringBootAssignmentsApplicationTests.java
@@ -0,0 +1,13 @@
+package in.aditya.springboot.springbootassignments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignmentsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
